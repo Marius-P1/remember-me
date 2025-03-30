caa0fc699bd3dfc164ebd8064e2930082d57c18f
Jibe LELEVIER <devme@MSI.(none)>
2025-03-30 10:19:14 +0200

    first page ok validé

--- app-front/src/App.vue
+++ app-front/src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-700 relative">
     <router-link to="/" class="absolute top-4 left-4 w-10 h-10">
-      <img
-        src="../public/icon.png"
-        alt="Icône"
-        class="w-full h-full object-contain"
-      />
+      <img src="/icon.png" alt="Icône" class="w-full h-full object-contain" />
     </router-link>
 
     <router-view />
